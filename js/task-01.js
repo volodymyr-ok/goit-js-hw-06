bfa74f4d406321c09f1515f994304d085af55e5f
Volodymyr Ok <v.shark.t@gmail.com>
2022-10-04 00:06:01 +0300

    Done first task

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categories = document.querySelectorAll('.item');
+
+console.log('Number of categories:', categories.length);
+
+categories.forEach(category => {
+  console.log('Category:', category.querySelector('h2').textContent);
+  console.log('Elements:', category.querySelectorAll('li').length);
+});
